de91a9d63fcc39144d6a000d8e4475d6ae15e2cc
cwhite911 <smortopahri@gmail.com>
2015-02-13 17:14:22 -0500

    Added basic map and event options

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,6 +3,68 @@
 angular.module('thePoppingDeadApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
     $scope.awesomeThings = [];
+    L.mapbox.accessToken = 'pk.eyJ1IjoiY3R3aGl0ZSIsImEiOiJRTHJWenVFIn0.CrPgidHVhZvg-DZ8CK_4zA';
+    var map = L.mapbox.map('map', 'ctwhite.l7618poc', {
+      zoomControl: false
+    });
+    $scope.newPlayer = function (){
+      var data = {
+        "type": "FeatureCollection",
+        "features": [
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+          -82.56827473640442,
+          35.60281152073829
+          ]
+        }
+      }
+      ]
+    };
+    L.geoJson(data, {
+      onEachFeature: function (feature, layer) {
+        console.log(layer.getLatLng())
+        // map.panTo(layer.getLatLng());
+
+      }
+    }).addTo(map);
+    // map.fitBounds([
+    //   [35.60300779515903, -82.56853491067886],
+    //   [35.602637054182395, -82.56800919771193]
+    //   ]);
+  }();
+
+
+
+  $scope.checkKey = function (e) {
+    console.log(e);
+    e = e || window.event;
+
+    if (e.keyCode === 38) {
+      // up arrow
+      e.preventDefault()
+      console.log('up')
+    }
+    else if (e.keyCode === 40) {
+      // down arrow
+      e.preventDefault()
+      console.log('down')
+    }
+    else if (e.keyCode === 37) {
+      // left arrow
+      e.preventDefault()
+      console.log('left')
+    }
+    else if (e.keyCode === 39) {
+      // right arrow
+      e.preventDefault()
+      console.log('right')
+    }
+
+  }
 
     $http.get('/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
