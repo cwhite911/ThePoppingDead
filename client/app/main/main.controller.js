1ea4b974b216a065b56b3492b6d99388f04426fd
cwhite911 <smortopahri@gmail.com>
2015-02-13 23:57:21 -0500

    Added controls and bomb power

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,70 +1,100 @@
 'use strict';
 
 angular.module('thePoppingDeadApp')
-  .controller('MainCtrl', function ($scope, $http, socket) {
+  .controller('MainCtrl', function ($scope, $http, socket, $interval) {
     $scope.awesomeThings = [];
     L.mapbox.accessToken = 'pk.eyJ1IjoiY3R3aGl0ZSIsImEiOiJRTHJWenVFIn0.CrPgidHVhZvg-DZ8CK_4zA';
-    var map = L.mapbox.map('map', 'ctwhite.l7618poc', {
+    var map,
+        playerStart,
+        playerTrail,
+        trailArray,
+        player;
+
+    map = L.mapbox.map('map', 'ctwhite.l7618poc', {
       zoomControl: false
     });
-    $scope.newPlayer = function (){
-      var data = {
-        "type": "FeatureCollection",
-        "features": [
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-          -82.56827473640442,
-          35.60281152073829
-          ]
-        }
-      }
-      ]
-    };
-    L.geoJson(data, {
-      onEachFeature: function (feature, layer) {
-        console.log(layer.getLatLng())
-        // map.panTo(layer.getLatLng());
-
-      }
-    }).addTo(map);
-    // map.fitBounds([
-    //   [35.60300779515903, -82.56853491067886],
-    //   [35.602637054182395, -82.56800919771193]
-    //   ]);
-  }();
 
 
+  playerStart = L.latLng(35.60281152073829, -82.56827473640442);
+  player = L.marker(playerStart);
+
+  trailArray = [playerStart];
+
+
+
+
+ $scope.moveCounter = 0;
+ $scope.bombCount = 3;
+ function getRandomColor() {
+   var letters = '0123456789ABCDEF'.split('');
+   var color = '#';
+   for (var i = 0; i < 6; i++ ) {
+     color += letters[Math.floor(Math.random() * 16)];
+   }
+   return color;
+ }
 
   $scope.checkKey = function (e) {
     console.log(e);
     e = e || window.event;
+    e.preventDefault()
+    switch (e.keyCode){
+      case 38:
+        //up
+      case 40:
+        //down
+      case 37:
+        //left
+      case 39:
+        //right
+        player.setLatLng(map.getCenter());
+        if (trailArray.length === 2 && $scope.moveCounter === 1) {
+          playerTrail = L.polyline(trailArray, {
+            color: getRandomColor(),
+            dashArray: '3 10'
+          }
+        );
+        playerTrail.addTo(map);
+        }
+        else if (trailArray.length > 2){
+          playerTrail.addLatLng(map.getCenter());
+        }
+        else {
+          trailArray.push(map.getCenter());
+        }
+          $scope.moveCounter++;
+        break;
+      case 32:
+        if ($scope.bombCount){
+          $scope.bombCount--;
+          $scope.bomb;
+          $scope.bombRadius = 5;
+          $scope.bomb = L.circle(map.getCenter(), $scope.bombRadius, {
+            color: getRandomColor()
+          }).addTo(map);
+          $interval(function(){
+            if ($scope.bomb.getRadius() < 20) {
+              $scope.bombRadius+= 5;
+              $scope.bomb.setStyle(getRandomColor());
+              $scope.bomb.setRadius($scope.bombRadius);
+            }
+            else {
+              $scope.bombRadius = 5;
+              $scope.bomb.setRadius(0);
+              map.removeLayer($scope.bomb);
 
-    if (e.keyCode === 38) {
-      // up arrow
-      e.preventDefault()
-      console.log('up')
-    }
-    else if (e.keyCode === 40) {
-      // down arrow
-      e.preventDefault()
-      console.log('down')
-    }
-    else if (e.keyCode === 37) {
-      // left arrow
-      e.preventDefault()
-      console.log('left')
-    }
-    else if (e.keyCode === 39) {
-      // right arrow
-      e.preventDefault()
-      console.log('right')
+            }
+
+          }, 75, 5);
+        }
+
+      default:
+
     }
 
-  }
+  };
+
+  player.addTo(map);
 
     $http.get('/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
